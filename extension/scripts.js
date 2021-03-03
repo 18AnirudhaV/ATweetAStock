2f3862de454bb20f207651568da07f9c676b6215
mustafa-tariqk <mustafatariqk@gmail.com>
2021-03-02 20:25:35 -0500

    Added input options, need to change to console log options.

--- extension/scripts.js
+++ extension/scripts.js
@@ -1,1 +1,15 @@
 // Handles your frontend UI logic.
+const pos = "👍";
+const neg = "👎";
+const neu = "🤷";
+
+var input = window.prompt("");
+if (input == "pos") {
+    document.body.innerHTML = document.body.innerHTML.replace('emoji', pos);
+} else if (input == "neg") {
+    document.body.innerHTML = document.body.innerHTML.replace('emoji', neg);
+} else {
+    document.body.innerHTML = document.body.innerHTML.replace('emoji', neu);
+}
+
+
